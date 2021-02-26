e2f25fd6c3650363aec1f97f48266b07b077606e
minamibata <66579505+minamibata@users.noreply.github.com>
2021-02-26 20:26:25 +0900

    commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,26 @@ class TasksController < ApplicationController
     def index
         @tasks = Task.all
     end
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+        redirect_to tasks_path
+    end
+
+    def destroy
+        redirect_to tasks_path
+    end
+
+# どういう記述なのかを答える
+    private
+    def task_params
+        params.require(:task).permit(:title)
+    end
 end
